9d40b04cd0b9c0f524b0f0045bea589777a06b0d
yang lizhi <271071524@qq.com>
2017-02-25 21:26:14 +0800

    布局完成

---
+++ js/home/login.js
@@ -0,0 +1,6 @@
+/**
+ * Created by 琴瑟 on 2017/2/25.
+ */
+define(['jquery'],function($){
+    console.log(111);
+});
---
+++ js/main.js
@@ -0,0 +1,35 @@
+/**
+ * Created by 琴瑟 on 2017/2/25.
+ */
+requirejs.config({
+    baseUrl:'/',
+    paths:{
+        //第三方库的配置
+        jquery:'lib/jquery/jquery.min',
+        bootstrap:'lib/bootstrap/js/bootstrap.min',
+        //自己写的路径配置
+        userList:'js/user/list',
+        userProfile:'js/user/profile'
+    },
+    shim:{
+        bootstrap:{
+            deps:['jquery']
+        }
+    }
+});
+
+//所有的页面都需要这两个js,先加载他们
+require(['jquery','bootstrap']);
+
+//这里获取页面的pathname，然后对应的加载js
+(function(window){
+    var pathname=window.location.pathname;
+    switch(pathname){
+        case'html/user/list.html':
+            require(['userList']);
+            break;
+        case'html/user/profile.html':
+            require(['userProfile']);
+            break;
+    }
+});(window);
