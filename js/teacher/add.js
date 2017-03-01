610e8cda5f782d44c6eeedbf894faa9befde91e3
yang lizhi <271071524@qq.com>
2017-03-01 14:55:17 +0800

    讲师状态修改&添加讲师

--- js/teacher/add.js
+++ js/teacher/add.js
@@ -3,4 +3,20 @@
  */
 define(['jquery','common','nprogress'],function($,undefined,nprogress){
     nprogress.done();
+    $('#teacher-add-form').on('submit',function(){
+        //添加讲师
+        $.ajax({
+            url:'/v6/teacher/add',
+            type:'post',
+            data:$(this).serialize(),
+            success:function(data){
+                //添加成功，跳转到讲师列表页
+                if(data.code==200){
+                    location.href='/html/teacher/list.html';
+                }
+            }
+        });
+    });
+
+    return false;
 });
--- js/teacher/list.js
+++ js/teacher/list.js
@@ -4,13 +4,28 @@
 define(['jquery','common','nprogress','template'],function($,undefined,nprogress,template){
     nprogress.done();
 
+    //讲师列表数据缓存
+    var teacherListCache;
+    try{
+        teacherListCache=JSON.parse(localStorage.getItem('teacherListCache'));
+    }catch(e){}
+    /**
+     * 如果存在缓存数据优先使用缓存
+     * 否则发送ajax请求重新获取数据，然后进行缓存
+     */
     //渲染讲师列表
-    $.get('/v6/teacher',function(data){
-        if(data.code==200){
-            var html=template('teacher-list-tpl',{list:data.result});
-            $('#teacher-list-tbody').html(html);
-        }
-    });
+    if(teacherListCache){
+        var html=template('teacher-list-tpl',{list:teacherListCache});
+        $('#teacher-list-tbody').html(html);
+    }else{
+        $.get('/v6/teacher',function(data){
+            if(data.code==200){
+                localStorage.setItem('teacherListCache',JSON.stringify(data.result));
+                var html=template('teacher-list-tpl',{list:data.result});
+                $('#teacher-list-tbody').html(html);
+            }
+        });
+    }
 
     //通过事件委托的方式给动态生成的a标签绑定点击事件
     //然后获取讲师详细信息并展示
@@ -21,6 +36,25 @@ define(['jquery','common','nprogress','template'],function($,undefined,nprogress
             if(data.code==200){
                 var html=template('teacher-view-tpl',data.result);
                 $("#teacherModal").html(html);
+            }
+        });
+    });
+    //讲师状态修改
+    $('#teacher-list-tbody').on('click','.teacher-status',function(){
+        var $self=$(this);
+        $.ajax({
+            url:'/v6/teacher/handle',
+            type:'post',
+            data:{
+                tc_id:$(this).parent().attr('data-id'),
+                tc_status:$(this).parent().attr('data-status')
+            },
+            success:function(data){
+                if(data.code==200){
+                    //得到修改后的状态，使用该状态修改按钮名称&父元素的data-status属性值
+                    $self.html(data.result.tc_status==0? '开启':'注销');
+                    $self.parent().attr('data-status',data.result.tc_status);
+                }
             }
         });
     });
